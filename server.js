f58df6cb09ddca19176e64fdd4fb0a4129162b70
CrossZeigler <70531290+CrossZeigler@users.noreply.github.com>
2020-09-24 12:22:46 -0400

    changed url to be /api, changed post message to be under POST request detected instead of GET request

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
-    /* Sending stuff back */
-    /* res.status(200).send('Hello World'); */
   })
   .post((req, res) => {
     console.log('POST request detected');
+    /* Sending stuff back, GET requests to get /api */
+    res.status(200).send('Hello World');
     console.log('Form data in res.body', req.body);
   });
 
