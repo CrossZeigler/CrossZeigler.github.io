305df44203b304be70edb668eb8d6ccfabdfa64a
CrossZeigler <70531290+CrossZeigler@users.noreply.github.com>
2020-09-23 20:16:26 -0400

    changed ids, labels, and names to have appropriate values and exist
    Added send to server.js
    Changed padding in flex outer

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static('public'));
 app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
+    /* Sending stuff back */
+    /* res.status(200).send('Hello World'); */
   })
   .post((req, res) => {
     console.log('POST request detected');
